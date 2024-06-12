112af9807b8b62d2792b0b23fe1f0b623ff9485c
Menelik Mesa <menelik.mesa@gmail.com>
2024-06-12 11:52:12 -0400

    initializing spring framework project

---
+++ src/main/java/com/spring/spring_boot_fire/SpringBootFireApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_boot_fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spring_boot_fire/SpringBootFireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_boot_fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
