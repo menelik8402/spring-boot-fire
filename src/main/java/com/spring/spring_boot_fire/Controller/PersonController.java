79fc40d34ad0cf8a5511c8792cc9924f13591e9c
Menelik Mesa <menelik.mesa@gmail.com>
2024-06-20 01:54:53 -0400

    handle kafka service into app

--- src/main/java/com/spring/spring_boot_fire/Controller/PersonController.java
+++ src/main/java/com/spring/spring_boot_fire/Controller/PersonController.java
@@ -1,13 +1,8 @@
 package com.spring.spring_boot_fire.Controller;
 
 import com.spring.spring_boot_fire.Entity.Person;
-import com.spring.spring_boot_fire.Entity.PersonOlder;
-import com.spring.spring_boot_fire.Service.PersonOlderService;
 import com.spring.spring_boot_fire.Service.PersonService;
-import com.spring.spring_boot_fire.mapper.PersonMapper;
 import com.spring.spring_boot_fire.model.PersonRequest;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,11 +16,6 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @Autowired
-    private PersonOlderService personOlderService;
-
-    @Autowired
-    private  PersonMapper personMapper ;
 
     @RequestMapping
     public List<Person> getAll(){
@@ -34,12 +24,7 @@ public class PersonController {
 
     @PostMapping
     public void saveOrUpdate(@RequestBody PersonRequest personRequest){
-        Person person = this.personMapper.personRequestToPerson(personRequest);
-        if(person.getAge() > 100){
-            PersonOlder personOlder = this.personMapper.personRequestPersonOlder(personRequest);
-            this.personOlderService.saveOrUpdatePerson(personOlder);
-        }
-        this.personService.saveOrUpdatePerson(person);
+             this.personService.saveOrUpdatePerson(personRequest);
     }
 
     @DeleteMapping("/{personId}")
